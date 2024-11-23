ae6f70ba08f40115a3a9951c0990addfa2913bab
melanieloveslinux <keltikangel236@gmail.com>
2024-11-23 12:08:53 +0000

    Project Initialization

---
+++ main.py
@@ -0,0 +1,20 @@
+# Imports
+import tkinter as tk
+
+
+
+# Functions
+
+
+
+# Classes
+class game():
+	def __init__(self):
+		self.root = tk.Tk()
+		self.root.mainloop()
+
+
+
+# Main
+typyer = game
+typyer()
---
+++ readme.md
@@ -0,0 +1,3 @@
+TyPyer
+===
+TyPyer ("Tie-pie-er") is a speed typing game made to be lightweight and simplistic.
